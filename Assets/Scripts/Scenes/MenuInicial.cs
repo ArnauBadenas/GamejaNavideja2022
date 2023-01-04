51f8380b7e6f9fd2a8bf7ad7b800231a3ea5e580
GameOnes <97537553+GameOnes@users.noreply.github.com>
2023-01-04 11:58:43 +0100

    .

---
+++ Assets/Scripts/Scenes/MenuInicial.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play()
+    {
+        Debug.Log("jugar");
+        SceneManager.LoadScene("GuanGuan");
+    }
+
+    // Update is called once per frame
+    public void Salir()
+    {
+        Debug.Log("cerrar juego");
+        Application.Quit();
+    }
+}
